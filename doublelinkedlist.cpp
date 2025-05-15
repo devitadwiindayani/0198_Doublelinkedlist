4cd9d7ce267367fd6a80ea34c1f11222e0c352a3
devitadwiindayani <devitadwiindayani@gmail.com>
2025-05-15 10:50:55 +0700

    menambahkan step 1-3 pada fungsi traverse

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -135,5 +135,18 @@ public:
             cout << "\nList is empty" << endl;
             return;
         }
+        // Step 1: Mark first node as currentNode
+        Node *currentNode =START;
+        // Step 2: Repeat until  currentNode == NULL
+        cout << "\nRecords in ascending order of roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+            // Step 3: MOve to next node
+            currentNode = currentNode->next;
+            i++;
+        }
+        
     }
 } 
