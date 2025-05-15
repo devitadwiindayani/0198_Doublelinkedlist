8f8c661db0794c2f69188e5bb8dc39fe107bf3db
devitadwiindayani <devitadwiindayani@gmail.com>
2025-05-15 09:54:10 +0700

    menambahkan void addnode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -20,4 +20,13 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+    }
 }
